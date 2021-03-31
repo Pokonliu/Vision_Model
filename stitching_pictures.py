af43ff0db0beac60e46daa3b4db5ac46a7b94ca5
liutianyang <302702801@qq.com>
2021-04-01 00:25:13 +0800

    1.优化拼接图片程序

--- stitching_pictures.py
+++ stitching_pictures.py
@@ -4,6 +4,7 @@ import cv2.cv2 as cv2
 import utils
 import pandas
 import math
+import random
 
 
 ANGLE = 56.5
@@ -12,22 +13,15 @@ width = 31
 length = 74
 
 
-def get_coordinate(path, angle):
+def get_coordinate(src, angle):
     def mouse_callback(event, x, y, flags, param):
-        nonlocal effectiveness
         if event == cv2.EVENT_LBUTTONDOWN:
             print("当前坐标：({},{})".format(x, y))
             coordinates.append([x, y])
             cv2.circle(rotate_img, (x, y), 1, (255, 0, 255), 1)
             cv2.putText(rotate_img, "({},{})".format(x, y), (x + 4, y - 4), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 1)
-        if event == cv2.EVENT_RBUTTONDOWN:
-            effectiveness = "invalid"
-            cv2.putText(rotate_img, "invalid", (x + 4, y - 4), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 1)
         cv2.imshow("Calibration", rotate_img)
     coordinates = []
-    effectiveness = "valid"
-    src = cv2.imread(path, cv2.IMREAD_COLOR)
-    print(path)
     rotate_img = utils.rotate_image(src, angle)
 
     cv2.namedWindow("Calibration")
@@ -35,73 +29,174 @@ def get_coordinate(path, angle):
     cv2.imshow("Calibration", rotate_img)
 
     key = cv2.waitKey(0)
-    # RUNNING
+    # 找起始
     if key == 13:
-        print(coordinates, effectiveness)
-        return True, coordinates[:2], effectiveness
-    # QUIT
+        print(coordinates)
+        return True, coordinates[0]
+    # 跳过
     if key == 27:
         return False, None, None
 
 
+def get_offset_table(path):
+    result = {}
+    data = pandas.read_csv(path)
+    for index, item in data.iterrows():
+        result[item["file_name"]] = item["x_offset"]
+    return result
 
-file_224 = r"F:\learning\Data\t\temp\DLC224\R926.bmp"
-file_232 = r"F:\learning\Data\t\temp\DLC232\R926.bmp"
-file_240 = r"F:\learning\Data\t\temp\DLC240\R926.bmp"
-file_248 = r"F:\learning\Data\t\temp\DLC248\R926.bmp"
-file_256 = r"F:\learning\Data\t\temp\DLC256\R926.bmp"
-# get_coordinate(file_224, ANGLE)
 
-hash_table = {"DLC224": file_224,
-              "DLC232": file_232,
-              "DLC240": file_240,
-              "DLC248": file_248,
-              "DLC256": file_256}
+def get_group_image(root_path, start_index, cycle, line_num, direction):
+    """
 
-data = pandas.read_csv("./left.csv")
-print(data.head(5))
+    :param root_path:
+    :param start_index:
+    :param cycle:
+    :param line_num:
+    :param direction:
+    :return:
+    """
+    result = {}
+    total_file_set = set(os.listdir(root_path))
+    # 过滤掉头照片
+    discard_file_set = set(x for x in total_file_set if "DTR" in x or "DTL" in x)
+    valid_file_set = total_file_set.difference(discard_file_set)
+    print(len(valid_file_set))
 
-offset_table = {}
+    for i in range(len(valid_file_set) // 2):
+        dir_name = "D{}C".format(direction) + str(start_index)
+        dir_path = os.path.join(root_path, dir_name)
+        if os.path.exists(dir_path):
+            for root, dirs, files in os.walk(dir_path):
+                file_name = "R{}.bmp".format(str(line_num))
+                if file_name in files:
+                    result[dir_name] = cv2.imread(os.path.join(root, file_name), cv2.IMREAD_COLOR)
+        else:
+            print("{} not exist, please check original data".format(dir_name))
+        start_index += cycle if direction == "L" else -cycle
+    print(result)
+    return result
 
-for index, item in data.iterrows():
-    offset_table[item["file_name"]] = item["x_offset"]
 
-image = cv2.imread(r"F:\learning\Data\t\temp\DLC216\R926.bmp", cv2.IMREAD_COLOR)
-r_img = utils.rotate_image(image, ANGLE)
-cropped_img = r_img[start_coordinate[1]: start_coordinate[1] + length, start_coordinate[0]: start_coordinate[0] + 1]
+# 读取一行数据并进行拼接API
+def stitching_picture(group_of_picture: dict, direction, start_coordinates, offset_table, angle, width, length):
+    """
+    通过一组图片集合来拼接出一张无重复图片
+    :param group_of_picture: eg: {序号: 图片, 序号: 图片, .....]
+    :param direction: 'R' or 'L'
+    :param start_coordinates: (x, y)
+    :param offset_table: [csv中读出的offset数据]
+    :param angle: 旋转角度
+    :param width: 针格像素宽度
+    :param length: 针格像素高度
+    :return:
+    """
+    # 初始化最终输出的图片用于堆叠
+    target_image = None
+    print(start_coordinates)
+    for index, image in group_of_picture.items():
+        offset = offset_table[index]
+        rotated_img = utils.rotate_image(image, angle)
+        count = offset / width
 
-target_image = cropped_img
+        choose = random.random() > 0.5
 
-for k, v in hash_table.items():
-    print("当前图片:", k)
-    image = cv2.imread(v, cv2.IMREAD_COLOR)
-    r_img = utils.rotate_image(image, ANGLE)
+        cropped_count = math.ceil(count) if choose else math.floor(count)
+        next_offset = abs(cropped_count - count)
 
-    offset = offset_table[k]
-    print("偏移量:", offset)
-
-    count = offset / width
-    print("偏移个数:", count)
-
-    # 取上整
-    cropped_count = math.ceil(count)
-    print("偏移个数取整:", cropped_count)
-
-    next_offset = cropped_count - count
-    print("下次需要移动距离:", next_offset)
-
-    print("本次起点坐标:", start_coordinate)
-    cropped_img = r_img[start_coordinate[1]: start_coordinate[1] + length, start_coordinate[0] - width * cropped_count: start_coordinate[0]]
-    target_image = np.concatenate((cropped_img, target_image), axis=1)
-    print("当前图片形状:", target_image.shape)
-
-    # 对下一次坐标修正
-    start_coordinate[0] -= int(next_offset * width)
-    print("下次起点坐标:", start_coordinate)
-
-    cv2.imshow("d", r_img)
-    cv2.imshow("c", cropped_img)
-    cv2.imshow("T", target_image)
+        if "L" == direction:
+            print("A", start_coordinates[1], start_coordinates[1] + length, start_coordinates[0] - width * cropped_count, start_coordinates[0])
+            cropped_img = rotated_img[start_coordinates[1]: start_coordinates[1] + length, start_coordinates[0] - width * cropped_count: start_coordinates[0]]
+            start_coordinates[0] += int(-next_offset * width if choose else next_offset * width)
+        else:
+            cropped_img = rotated_img[start_coordinates[1]: start_coordinates[1] + length, start_coordinates[0]: start_coordinates[0] + width * cropped_count]
+            start_coordinates[0] += int(next_offset * width if choose else - next_offset * width)
+        if type(target_image) == np.ndarray:
+            target_image = np.concatenate((cropped_img, target_image), axis=1)
+        else:
+            target_image = cropped_img
+    print(target_image.shape)
+    cv2.imshow('result', target_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    return target_image
 
+
+# file_224 = r"F:\learning\Data\t\temp\DLC224\R926.bmp"
+# file_232 = r"F:\learning\Data\t\temp\DLC232\R926.bmp"
+# file_240 = r"F:\learning\Data\t\temp\DLC240\R926.bmp"
+# file_248 = r"F:\learning\Data\t\temp\DLC248\R926.bmp"
+# file_256 = r"F:\learning\Data\t\temp\DLC256\R926.bmp"
+# # get_coordinate(file_224, ANGLE)
+#
+# hash_table = {"DLC224": file_224,
+#               "DLC232": file_232,
+#               "DLC240": file_240,
+#               "DLC248": file_248,
+#               "DLC256": file_256}
+#
+# data = pandas.read_csv("./left.csv")
+# print(data.head(5))
+#
+# offset_table = {}
+#
+# for index, item in data.iterrows():
+#     offset_table[item["file_name"]] = item["x_offset"]
+#
+# image = cv2.imread(r"F:\learning\Data\t\temp\DLC216\R926.bmp", cv2.IMREAD_COLOR)
+# r_img = utils.rotate_image(image, ANGLE)
+# cropped_img = r_img[start_coordinate[1]: start_coordinate[1] + length, start_coordinate[0]: start_coordinate[0] + 1]
+#
+# target_image = cropped_img
+#
+# for k, v in hash_table.items():
+#     print("当前图片:", k)
+#     image = cv2.imread(v, cv2.IMREAD_COLOR)
+#     r_img = utils.rotate_image(image, ANGLE)
+#
+#     offset = offset_table[k]
+#     print("偏移量:", offset)
+#
+#     count = offset / width
+#     print("偏移个数:", count)
+#
+#     # 取上整
+#     cropped_count = math.ceil(count)
+#     print("偏移个数取整:", cropped_count)
+#
+#     next_offset = cropped_count - count
+#     print("下次需要移动距离:", next_offset)
+#
+#     print("本次起点坐标:", start_coordinate)
+#     cropped_img = r_img[start_coordinate[1]: start_coordinate[1] + length, start_coordinate[0] - width * cropped_count: start_coordinate[0]]
+#     target_image = np.concatenate((cropped_img, target_image), axis=1)
+#     print("当前图片形状:", target_image.shape)
+#
+#     # 对下一次坐标修正
+#     start_coordinate[0] -= int(next_offset * width)
+#     print("下次起点坐标:", start_coordinate)
+#
+#     cv2.imshow("d", r_img)
+#     cv2.imshow("c", cropped_img)
+#     cv2.imshow("T", target_image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    left_offset_table = get_offset_table("./Template/left.csv")
+    right_offset_table = get_offset_table("./Template/right.csv")
+    image_table = get_group_image(root_path=r"F:\learning\Data\t\temp", start_index=-2104, cycle=8, line_num=928, direction="L")
+    invalid_image_list = []
+    start_coordinate = None
+    for dir_name, img in image_table.items():
+        res, coordinate = get_coordinate(img, ANGLE)
+        if not res:
+            invalid_image_list.append(dir_name)
+        else:
+            start_coordinate = coordinate
+            break
+    for item in invalid_image_list:
+        image_table.pop(item)
+
+    stitching_picture(group_of_picture=image_table, direction="L", start_coordinates=start_coordinate, offset_table=left_offset_table, angle=ANGLE, width=width, length=length)
